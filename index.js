ef66de0b89fae2d3eb4ec459fa150835f3a41bda
Bobby <ivanoffboyan@gmail.com>
2024-02-05 22:09:58 +0100

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+let myLeads = ["lead1", "lead2", "lead3"]
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+
+localStorage.setItem("myName", "Bobby")
+
+let name = localStorage.getItem("myName")
+
+localStorage.clear()
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  renderLeads()
+})
+
+function renderLeads() {
+  let listItems = "" // Fix variable name
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'>
+          ${myLeads[i]}
+        </a>
+    </li>
+  `
+  }
+  ulEl.innerHTML = listItems // Fix assignment
+}
